7942166a8bd6fabbd1d16f607c77e8019c7145ac
scalerone <676119236@qq.com>
2019-09-21 21:42:54 +0800

    connect连接器的使用-用来映射关系 把原来的state映射成组件中的props属性

--- demo02/src/TodoList.js
+++ demo02/src/TodoList.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 
+import {connect} from 'react-redux'  //引入连接器
+
 import store from './redux/store'
+
+
 class TodoList extends Component {
 
     constructor(props){
@@ -13,7 +17,7 @@ class TodoList extends Component {
             <div>
                 <div>
 
-                    <input value={this.state.inputValue} />
+                    <input value={this.props.inputValue} />
                 <button>提交</button>
                 </div>
                 <ul>
@@ -23,5 +27,18 @@ class TodoList extends Component {
         );
     }
 }
+/*
+connect-连接器用来将redux管理的state数据映射成UI组件的一般属性的函数
+ 指定向TodoList传入哪些一般属性(属性值的来源就是store中的state)
+ */
+const stateToProps = (state)=>{
+    return {
+        inputValue : state.inputValue
+    }
+}
 
-export default TodoList;
+//使用连接器将组件暴露出去
+//这里的stateToProps代表一个映射关系
+// 映射关系就是把原来的state映射成组件中的props属性
+export default connect(stateToProps,null)(TodoList);
+// export default TodoList;
