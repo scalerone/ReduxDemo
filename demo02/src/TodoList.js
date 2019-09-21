b1f90f31f4393c14055df425d1b559663aa8f3ea
scalerone <676119236@qq.com>
2019-09-21 21:58:22 +0800

    connect-连接器将redux管理的包含diaptch代码的函数映射成UI组件的函数属性的函数 （如输入的onChange事件）

--- demo02/src/TodoList.js
+++ demo02/src/TodoList.js
@@ -12,12 +12,13 @@ class TodoList extends Component {
         //获取store的数据给state
         this.state = store.getState()
     }
+
     render() {
         return (
             <div>
                 <div>
 
-                    <input value={this.props.inputValue} />
+                    <input value={this.props.inputValue} onChange={this.props.inputChange} />
                 <button>提交</button>
                 </div>
                 <ul>
@@ -28,7 +29,7 @@ class TodoList extends Component {
     }
 }
 /*
-connect-连接器用来将redux管理的state数据映射成UI组件的一般属性的函数
+connect-连接器用来将redux管理的state数据映射成UI组件的一般属性（如输入框的值）
  指定向TodoList传入哪些一般属性(属性值的来源就是store中的state)
  */
 const stateToProps = (state)=>{
@@ -37,8 +38,25 @@ const stateToProps = (state)=>{
     }
 }
 
+/*
+connect-连接器用来将redux管理的包含diaptch代码的函数映射成UI组件的函数属性的函数
+（如输入的onChange事件）
+ */
+const dispatchToProps = (dispatch) =>{
+    return {
+        inputChange(e){
+            //派发action到store中：定义action 然后派发
+            //派发后就在reducer里边，编写对应的业务逻辑了
+            let action = {
+                type:'change_input',
+                value:e.target.value
+            }
+            dispatch(action)
+        }
+    }
+}
 //使用连接器将组件暴露出去
 //这里的stateToProps代表一个映射关系
 // 映射关系就是把原来的state映射成组件中的props属性
-export default connect(stateToProps,null)(TodoList);
+export default connect(stateToProps,dispatchToProps)(TodoList);
 // export default TodoList;
--- demo02/src/redux/reducer.js
+++ demo02/src/redux/reducer.js
@@ -5,5 +5,10 @@ const defalutState = {
 
 
 export default (state = defalutState,action) =>{
+    if(action.type === 'change_input'){
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.inputValue = action.value
+        return newState
+    }
     return state
 }
